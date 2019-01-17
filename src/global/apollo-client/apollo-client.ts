e2dde4dbe8ab8f9fc2d764179322ee9ec99a3534
David Maggi <david.maggi@orange.com>
2019-01-17 22:55:40 +0100

    [context] ApolloClient instance
    /!\ known bug dirty fix necessary, change first line of Optimism.js package to play nice with rollup
    https://github.com/apollographql/apollo-client/issues/4071
    
    /!\ useless import declaration of graphql-tag to avoid run-time error

---
+++ src/global/apollo-client/apollo-client.ts
@@ -0,0 +1,50 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+// import { withClientState } from 'apollo-link-state';
+import { ApolloLink } from 'apollo-link';
+import config from '../../app.config.json';
+
+// import declaration avoid run-time error with import
+import _gql from "graphql-tag";
+
+let instance:ApolloClient<any>;
+
+function getApolloClientInstance():ApolloClient<any> {
+    if (!instance) {
+        // initialize instance
+        instance = new ApolloClient({
+            link: ApolloLink.from([
+                onError(({ graphQLErrors, networkError }) => {
+                  if (graphQLErrors) graphQLErrors.map(({ message, locations, path }) => console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
+                  if (networkError) console.log(`[Network error]: ${networkError}`);
+                }),
+                new HttpLink({
+                  uri: config["apollo-client"].httpLinkConfig.uri,
+                  credentials: config["apollo-client"].httpLinkConfig.credentials
+                }),
+                // withClientState({
+                //     defaults: {
+                //       isConnected: true
+                //     },
+                //     resolvers: {
+                //         Mutation: {
+                //             updateNetworkStatus: (_, { isConnected }, { cache }) => {
+                //               cache.writeData({ data: { isConnected }});
+                //               return null;
+                //             }
+                //         }
+                //     }
+                // })
+            ]),
+            cache: new InMemoryCache()
+        });
+
+        console.info('ApolloClient instanciated');
+    }
+    
+    return instance;
+};
+
+export default getApolloClientInstance;
